497fabcc48a084af31a1ce59294d3e0efa47633d
athlrajm <athulrajm333@gmail.com>
2024-03-28 02:42:35 +0530

    final commit

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -13,7 +13,7 @@ function LoginPage() {
        <div className='one'>
         <img className="img1" src="https://i.pinimg.com/736x/4e/f5/d8/4ef5d838ba0931d3b920641918b4bbae.jpg" alt=''/>
         <h1 className='l1'>LOGIN</h1>
-        <p className='p1'>Our Todo App is designed to help you organize your tasks efficiently.<br></br> Whether it's managing your daily chores, keeping track of work assignments,<br></br> or simply jotting down ideas, our app provides a seamless experience <br></br>to keep you organized and productive.</p>
+        <p className='pp1'>Our Todo App is designed to help you organize your tasks efficiently.<br></br> Whether it's managing your daily chores, keeping track of work assignments,<br></br> or simply jotting down ideas, our app provides a seamless experience <br></br>to keep you organized and productive.</p>
         <br></br>
         <button className="bt1" onClick={signInUser}>
   <img src={GoogleIcon} alt="Google" className="google-icon" /> Sign in using Google
